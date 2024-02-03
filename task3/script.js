596f8c830c99491e60ed9e87c18053fe760b6d79
NatalyaGalyamova <peoplejoyster@gmail.com>
2024-02-03 13:30:06 -0500

    js revised

--- task3/script.js
+++ task3/script.js
@@ -1,33 +1,12 @@
-function useRequest(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  
-  xhr.onload = function() {
-    if (xhr.status != 200) {
-      console.log('Статус ответа: ', xhr.status);
-    } else {
-      const result = JSON.parse(xhr.response);
-      if (callback) {
-        callback(result);
-      }
-    }
-  };
-  
-  xhr.onerror = function() {
-    console.log('Ошибка! Статус ответа: ', xhr.status);
-  };
-  
-  xhr.send();
-};
-
 const resultNode = document.querySelector('.j-result');
 
 const btnNode = document.querySelector('.btn');
 
-const value = document.querySelector('input').value;
-
-function displayResult(item) {
-  const card = `
+function displayResult(result) {
+  let cards = '';
+  
+  result.forEach(item => {
+    const cardBlock = `
       <div class="card">
         <img
           src="${item.thumbnailUrl}"
@@ -36,26 +15,37 @@ function displayResult(item) {
         <p>${item.title}</p>
       </div>
     `;
+    cards = cards + cardBlock;
+  });
     
-  resultNode.innerHTML = card;
-}
-
-const checkNumber = (value) => {
-    let url = 'https://jsonplaceholder.typicode.com/photos?_limit=';
-
-    let message = '';
-
-    if(value < 1 || value > 10){
-        message = 'число вне диапазона от 1 до 10';
-        resultNode.innerHTML = message;
-    } else {
-        url += value;
-        useRequest(url, displayResult);
-    } 
+  resultNode.innerHTML = cards;
 }
 
 btnNode.addEventListener('click', () => {
-    checkNumber(value); 
+  const value = document.querySelector('input').value;
+
+  if(value < 1 || value > 10){
+    resultNode.innerHTML = 'число вне диапазона от 1 до 10';
+  } else {
+    const url = `https://jsonplaceholder.typicode.com/photos?_limit=${value}`;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    
+    xhr.onload = function() {
+      if (xhr.status != 200) {
+        console.log('Статус ответа: ', xhr.status);
+      } else {
+        const result = JSON.parse(xhr.response);
+        displayResult(result)
+      }
+    };
+    
+    xhr.onerror = function() {
+      console.log('Ошибка! Статус ответа: ', xhr.status);
+    };
+    
+    xhr.send();
+  } 
 })
 
 
